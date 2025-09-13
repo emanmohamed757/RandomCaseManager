290132b9aa80472085d314f993669539a909ccac
Eman <jamalyarfoor@gmail.com>
2025-09-13 19:40:03 +0500

    Add comment to pretend to have changes, so that pull request can be created

--- RandomCaseManager/Program.cs
+++ RandomCaseManager/Program.cs
@@ -14,7 +14,7 @@ namespace RandomCaseManager
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+            Application.EnableVisualStyles();//
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
